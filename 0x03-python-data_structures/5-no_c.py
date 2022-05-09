57cbe360a15ed5d67f2aa623d450873a148ac410
alejuran <alejandrourn@gmail.com>
2022-05-09 15:50:22 -0500

    no c commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    characters = "cC"
-    my_string = ''.join(i for i in my_string if i not in characters)
-    return my_string
+    n_str = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            n_str += i
+    return n_str
