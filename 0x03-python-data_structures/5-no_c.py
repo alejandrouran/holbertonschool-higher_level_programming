40708a7ba6652a2d18059d5b488e3acd62aba9c5
alejuran <alejandrourn@gmail.com>
2022-05-09 20:34:48 -0500

    update commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    n_str = ''
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            n_str += i
-    return n_str
+    n_str = ''.join(i for i in my_string if i != "c" and i != "C")
+    return(n_str)
