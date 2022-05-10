36e53fca6c53b788aabd389a931e4870b2627f68
alejuran <alejandrourn@gmail.com>
2022-05-10 16:48:00 -0500

    best commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    else:
-        return max(a_dictionary, key=a_dictionary.get)
+    if a_dictionary:
+        return max(zip(a_dictionary.values(),
+            a_dictionary.keys()))[1]
+
