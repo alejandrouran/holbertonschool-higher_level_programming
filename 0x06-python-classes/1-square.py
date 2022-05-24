00a31c019ed67042db7e9742c65b5eba3e637c24
alejandro <alejandrourn@gmail.com>
2022-05-24 16:32:53 -0500

    update square commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
+    """new object of class Square"""
 
     def __init__(self, size):
+        """Initialize a square"""
 
         self.__size = size
