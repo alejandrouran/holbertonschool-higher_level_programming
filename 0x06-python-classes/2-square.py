9fbe751501a191726a0adf0b9a463028955dafc5
alejandro <alejandrourn@gmail.com>
2022-05-24 17:34:53 -0500

    update task commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""Defines a Square"""
+
+
 class Square:
+    """new object of class Square"""
 
     def __init__(self, size=0):
+        """initialize Square"""
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be <= 0")
-        
+            raise ValueError("size must be >= 0")
         self.__size = size
