3fd6c7173c645ccc07555ae7432b483a14e1c012
alejandro <alejandrourn@gmail.com>
2022-05-24 19:02:42 -0500

    square five commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines a Square"""
+
+
+class Square:
+    """new object of class Square"""
+
+    def __init__(self, size=0):
+        """initialize Square"""
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print("")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
