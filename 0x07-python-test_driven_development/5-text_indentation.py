624a91b49a417f8d6eb7ef9a4970f33293de20d7
alejandro <alejandrourn@gmail.com>
2022-05-30 18:14:13 -0500

    update text commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """
 
+    the "5-text_indentation" module
+    prints a text with 2 new lines after each of
+    these characters: ., ? and :
+
 
 """
 
 
 def text_indentation(text):
+    """prints the characters
+    {'.', '?', ':'}"""
 
     if type(text) is not str:
         raise TypeError("text must be a string")
