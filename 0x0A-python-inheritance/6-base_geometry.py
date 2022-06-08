23eb6016ff183ae41b296dd0c0ae45573cc8a21b
alejandro <alejandrourn@gmail.com>
2022-06-07 20:37:16 -0500

    base geometry commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""base_geometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
