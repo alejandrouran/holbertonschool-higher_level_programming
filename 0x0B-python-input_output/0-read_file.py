778ade73dc87c83e8420ebe17388c1ec9f985d6b
alejandro <alejandrourn@gmail.com>
2022-06-08 13:38:08 -0500

    read one commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""the function read_file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
