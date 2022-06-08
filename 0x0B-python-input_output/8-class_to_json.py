af4b42d4b0de5febc0931ae352989722552648c4
alejandro <alejandrourn@gmail.com>
2022-06-08 17:51:11 -0500

    class commit

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""the function class_to_json"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description\
+        with simple data structure """
+    return obj.__dict__
