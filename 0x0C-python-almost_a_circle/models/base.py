82df4eed7e3c441adbf903505e55fb56333eb795
alejuran <alejandrourn@gmail>
2022-06-14 20:06:44 -0500

    update base commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ This class will be the “base” of all other classes in this project
 import json
 import os
 from fileinput import filename
+from logging import exception
 
 
 class Base:
@@ -22,7 +23,10 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or list_dictionaries == []:
+        """
+        ...
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
@@ -32,11 +36,11 @@ class Base:
         """
         ...
         """
-        if list_objs is None or list_objs == []:
+        if list_objs is None or len(list_objs) == 0:
             json_s = "[]"
         else:
-            json_s = cls.to_json_string([element.to_dictionary()
-                                         for element in list_objs])
+            json_s = cls.to_json_string(
+                [element.to_dictionary() for element in list_objs])
         filename = f"{cls.__name__}.json"
         with open(filename, "w") as file:
             file.write(json_s)
@@ -46,7 +50,7 @@ class Base:
         """
         ...
         """
-        if json_string is None or json_string == []:
+        if type(json_string) is not str or len(json_string) == 0:
             return []
         else:
             return json.loads(json_string)
@@ -58,7 +62,7 @@ class Base:
         """
         if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
-        else:
+        elif cls.__name__ == "Square":
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
@@ -68,13 +72,13 @@ class Base:
         """
         ...
         """
-        filename = cls.__name__ + ".json"
-        l = []
-        try:
-            with open(filename, 'r') as f:
-                l = cls.from_json_string(f.read())
-            for i, e in enumerate(l):
-                l[i] = cls.create(**l[i])
-        except:
-            pass
-        return l
+        filename = f"{cls.__name__}.json"
+        new_l = []
+        new_dict = []
+        if os.path.exists(filename):
+            with open(filename, 'r') as file:
+                j = file.read()
+                new_dict = cls.from_json_string(j)
+                for i in new_dict:
+                    new_l.append(cls.create(**i))
+        return new_l
