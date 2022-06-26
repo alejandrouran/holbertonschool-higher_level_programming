2a9fcbd521076fd8b181ded1c323507a1b3752ec
alejuran <alejandrourn@gmail.com>
2022-06-26 16:20:55 -0500

    rect commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""class_rectangle"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        ...
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle"""
+    def __init__(self, width, height):
+        """rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        ...
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        ...
+        """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
