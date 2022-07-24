3e2e923d7ae6f0b70ebd1036fe8be4ccfc4312df
alejuran <alejandrourn@gmail.com>
2022-07-23 20:17:44 -0500

    max integer test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,5 +4,38 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    
+    """test max_integer"""
+    def test_def(self):
+        """test normal input"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_none(self):
+        """test empty imput"""
+        self.assertEqual(max_integer(), None)
+
+    def test_neg(self):
+        """test negative"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+    def test_neg_pos(self):
+        """test composed input"""
+        self.assertEqual(max_integer([1, -4, -5, 5, 3]), 5)
+
+    def test_non_int(self):
+        """
+        ...
+        """
+        with self.assertRaises(TypeError):
+            max_integer([1, '2', 3])
+
+    def test_float(self):
+        """
+        ...
+        """
+        self.assertEqual(max_integer([1, 2, 3.3]), 3.3)
+
+
+if __name__ == "__main__":
+    unittest.main()
