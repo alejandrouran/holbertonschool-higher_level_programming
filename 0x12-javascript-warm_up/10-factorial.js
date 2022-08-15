5ce5123e5cdff11ee475863dc831aed039e326d9
alejuran <alejandrouranarboleda@gmail.com>
2022-08-15 17:52:49 -0500

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 function factorial (n) {
-	if (n === || n === 1) {
-		return 1;
-	} else if (n) {
-		return factorial(n - 1) * n;
-	}
-	return 1;
+  if (isNaN(n) || n < 2) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-let n = parseInt(process.argv[2]);
-console.log(factorial(n));
+console.log(factorial(process.argv[2]));
