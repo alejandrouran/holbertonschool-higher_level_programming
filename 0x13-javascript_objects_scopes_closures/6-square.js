94ba2c9abc7c6e1f5742c8e5d9f3f2e0ad59d27a
alejuran <alejandrouranarboleda@gmail.com>
2022-08-16 17:41:41 -0500

    update square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,6 +4,7 @@ module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
+
   charPrint (c) {
     if (c === undefined) {
       this.print();
