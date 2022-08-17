463b1a3499389893188fc26ee16b8f79159114a1
alejuran <alejandrouranarboleda@gmail.com>
2022-08-17 15:55:10 -0500

    starwars_title commit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,10 @@
 const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/:id' + movieId;
-
-request (url, function (error, response, body) {
-	if (error) {
-	} else {
-		console.log(JSON.parse(body).title);
-	}
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
