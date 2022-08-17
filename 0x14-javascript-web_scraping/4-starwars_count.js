d1850310c7ebfd2513e34c5b57b8880e3f3542a1
alejuran <alejandrouranarboleda@gmail.com>
2022-08-17 17:53:25 -0500

    task four starwars_count commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const films = JSON.parse(body).results;
+    console.log(films.reduce((counter, film) => {
+      return film.characters.find((char) => char.endsWith('/18/'))
+        ? counter + 1
+        : counter;
+    }, 0));
+  }
+});
