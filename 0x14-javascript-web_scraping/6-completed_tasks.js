662e6af976e868f5cafa95014b2c44eca75e0c90
alejuran <alejandrouranarboleda@gmail.com>
2022-08-17 19:38:56 -0500

    six completed_tasks commit

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const all = JSON.parse(body);
+    let completed = {};
+    all.forEach((all) => {
+      if (all.completed && completed[all.userId] === undefined) {
+        completed[all.userId] = 1;
+      } else if (all.completed) {
+        completed[all.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
+});
