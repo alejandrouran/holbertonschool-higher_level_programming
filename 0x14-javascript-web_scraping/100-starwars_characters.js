661bd06957cc380df7232a5323ddb628e0743fc4
Alejandro Uran <98915073+alejuran@users.noreply.github.com>
2022-08-18 20:36:09 -0500

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,7 +4,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    characters.forEarch((character) => {
+    characters.forEach((character) => {
       request(character, function (error, response, body) {
         if (!error) {
           console.log(JSON.parse(body).name);
