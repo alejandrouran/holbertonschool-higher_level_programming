ddc1250a9be80f6f2035f153da13c4930e099ea8
alejuran <alejandrourn@gmail.com>
2022-09-02 16:55:54 -0700

    hbtn status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     import requests
     html = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(html.text__class__))
+    print("\t- type: {}".format(html.text.__class__))
     print("\t- content: {}".format(html.text))
