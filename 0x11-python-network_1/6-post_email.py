1483ae2329bccef1e5507714bdac94475b9de83d
alejuran <alejandrourn@gmail.com>
2022-09-02 17:22:43 -0700

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email address
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    pyl = {"email": sys.argv[2]}
+    response = requests.post(sys.argv[1], data=pyl)
+    print(r.text)
