13da87216560939a4a439e1ad3f44bbc8288c310
alejuran <alejandrourn@gmail.com>
2022-09-02 18:38:03 -0700

    my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+takes your GitHub credentials
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys import argv
+    url = 'https://api.github.com/user'
+    re = requests.get(url, auth=(argv[1], argv[2])).json()
+
+    print(re.get('id'))
