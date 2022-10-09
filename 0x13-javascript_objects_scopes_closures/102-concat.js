b611fc4f9b1a72e511422f92f39a282c5724f186
alejuran <alejandrourn@gmail.com>
2022-10-09 16:39:47 -0500

    concat advancer task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,4 +5,4 @@ let C = process.argv[4];
 let fs = require('fs');
 let textA = fs.readFileSync(A, 'utf8');
 let textB = fs.readFileSync(B, 'utf8');
-fs.writeFileSync(fileC, textA + textB);
+fs.writeFileSync(C, textA + textB);
