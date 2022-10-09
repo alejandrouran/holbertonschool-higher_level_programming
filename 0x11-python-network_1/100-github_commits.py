2100337b91204a0f1e18a97bcd51e292dc2f9c01
alejuran <alejandrourn@gmail.com>
2022-10-09 15:59:12 -0500

    github commits advanced

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
     commits = r.json()
-    for commits in commits[:10]:
+    for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
